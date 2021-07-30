d952ac063b3b8c1f76a0ef7221d2410348047792
Nathan Pemberton <nathanp@berkeley.edu>
2021-07-30 23:43:23 +0000

    Clean up lint issues

--- python/test/kv/kvBench.py
+++ python/test/kv/kvBench.py
@@ -2,25 +2,26 @@ import libff as ff
 import libff.kv
 import numpy as np
 import time
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
 
 # Test kv.put and kv.get
 def test_putAndget(kv, k, v, is_R, is_W):
     if is_R and is_W:
         start_time = time.time()
         kv.put(k, v)
-        result = kv.get(k)
+        kv.get(k)
         return time.time() - start_time
     if is_R:
         kv.put(k, v)
         start_time = time.time()
-        result = kv.get(k)
+        kv.get(k)
         return time.time() - start_time
     if is_W:
         start_time = time.time()
         kv.put(k, v)
         return time.time() - start_time
-    #assert np.array_equal(v, result)
+
 
 # Set up server outside
 def setup_mode(mode):
@@ -30,8 +31,8 @@ def setup_mode(mode):
         kv = ff.kv.Redis(pwd=ff.redisPwd, serialize=True)
     else:
         kv = ff.kv.Shmm()
-        #kv = ff.kv.Anna("127.0.0.1", "127.0.0.1", local=True)
     return kv
+
 
 # Helper function for testing mixed sizes of matrices
 def test_mixed(mode, klst, vlst, is_cold, is_R, is_W):
@@ -58,11 +59,12 @@ def test_mixed(mode, klst, vlst, is_cold, is_R, is_W):
         print("-"*30)
     return time_list
 
+
 # Helper function for testing one size of matrices
 def test_not_mixed(mode, klst, vlst, is_manyRW, is_cold, is_R, is_W):
     all_time_list = []
     if is_manyRW:
-            print("Test with mutiple reads and writes")
+        print("Test with mutiple reads and writes")
     for k, v in zip(klst, vlst):
         print("Test for " + k)
         with ff.testenv('time', mode):
@@ -93,20 +95,20 @@ def test_not_mixed(mode, klst, vlst, is_manyRW, is_cold, is_R, is_W):
             print("-"*30)
     return all_time_list
 
-''' 
-@para: 
-is_mixed: whether test with mixed sizes of matrices; 
-is_manyRW: whether test with reads and writes many times;
-is_cold: whether test without warming up the cache first. 
-'''
+
 def time_putAndget(mode, is_mixed=False, is_manyRW=False, is_cold=True, is_R=True, is_W=True):
-    #mode_list = ['direct', 'process', 'Anna']
+    '''
+    @para:
+    is_mixed: whether test with mixed sizes of matrices;
+    is_manyRW: whether test with reads and writes many times;
+    is_cold: whether test without warming up the cache first.
+    '''
     mode_list = ['direct', 'process', 'sharemem']
     assert (mode in mode_list)
     print('Test for ' + mode)
     klst = ["5by5", "100by100", "1000by1000", "5000by5000"]
-    vlst = [np.arange(25, dtype=np.uint32), np.arange(10000, dtype=np.uint32), \
-    np.arange(100000, dtype=np.uint32), np.arange(25000000, dtype=np.uint32)] # np.random is slow; np.range and np.shuffle
+    vlst = [np.arange(25, dtype=np.uint32), np.arange(10000, dtype=np.uint32),
+            np.arange(100000, dtype=np.uint32), np.arange(25000000, dtype=np.uint32)]
     assert len(klst) == len(vlst)
     if is_R and not is_W:
         print("Test only for reads")
@@ -118,7 +120,9 @@ def time_putAndget(mode, is_mixed=False, is_manyRW=False, is_cold=True, is_R=Tru
     else:
         return test_not_mixed(mode, klst, vlst, is_manyRW, is_cold, is_R, is_W)
 
+
 time_putAndget('sharemem', is_mixed=False, is_manyRW=False, is_cold=True, is_R=True, is_W=True)
+
 
 def plot_time_list(is_mixed=False, is_manyRW=False, is_cold=True, is_R=True, is_W=True):
     direct = time_putAndget('direct', is_mixed, is_manyRW, is_cold, is_R, is_W)
@@ -132,18 +136,3 @@ def plot_time_list(is_mixed=False, is_manyRW=False, is_cold=True, is_R=True, is_
         plt.show()
     else:
         return
-
-#plot_time_list(is_mixed=True)
-
-# warm up first without timing compared with cold cache
-# try to first complete all kvlst for one kv.
-# all the times (whole list, mean, std)
-# Mix some sizes of matrices; Read and Write multiple times; ...
-# because we need to initialize a Lattice class everytime to put value in Anna
-#print("%s seconds" % ((end_time - start_time) / 10))
-'''
-print("Test Redis run time for " + k)
-print(timeit.Timer("testRedis(k ,v)", globals=globals()).timeit(number=10))
-print("Test Anna run time for " + k)
-print(timeit.Timer("testAnna(k ,v)", globals=globals()).timeit(number=10))
-'''

80b79b57281067723fb137caf1e480e785f1f173
Nathan Pemberton <nathanp@berkeley.edu>
2021-08-03 15:28:14 -0700

    Working example of adapter and cutlass sgemm kernel through python

---
+++ examples/cutlass/kaasUtil/README.md
@@ -0,0 +1,6 @@
+### Compile shared library and kernel
+Make sure you are in the correct directory (cutlass/examples/00_basic_gemm).
+Run `sh buildall.sh`.
+
+### Pycuda test
+The temporary python test file I wrote is `test.py`.
---
+++ examples/cutlass/kaasUtil/test.py
@@ -0,0 +1,126 @@
+import pycuda.driver as cuda
+import pycuda.autoinit  # NOQA
+import numpy as np
+import ctypes as ct
+
+
+def loadKerns():
+    mod = cuda.module_from_file("./cutlass.cubin")
+    cutlassKern = mod.get_function("_ZN7cutlass6KernelINS_4gemm6kernel4GemmINS1_11threadblock12MmaPipelinedINS1_9GemmShapeILi128ELi128ELi8EEENS_9transform11threadblock22PredicatedTileIteratorINS_11MatrixShapeILi128ELi8EEEfNS_6layout8RowMajorELi1ENS8_30PitchLinearStripminedThreadMapINSD_16PitchLinearShapeILi8ELi128EEELi256ELi1EEELi1EEENS9_19RegularTileIteratorISC_fNSD_11ColumnMajorELi1ENS8_33TransposePitchLinearThreadMapSimtISI_EELi4EEENSA_INSB_ILi8ELi128EEEfSE_Li0ENSF_INSG_ILi128ELi8EEELi256ELi1EEELi1EEENSK_ISP_fSE_Li0ESR_Li4EEEfSE_NS4_9MmaPolicyINS1_4warp7MmaSimtINS6_ILi32ELi64ELi8EEEfSL_fSE_fSE_NSV_13MmaSimtPolicyINSB_ILi4ELi8EEENSD_19RowMajorInterleavedILi2EEENS6_ILi4ELi4ELi1EEEEELi1ELNS_16ComplexTransformE0ELS14_0EbEENSB_ILi4ELi0EEENSB_ILi0ELi0EEELi1EEENS_21NumericArrayConverterIffLi4ELNS_15FloatRoundStyleE2EEES1B_bEENS_8epilogue11threadblock8EpilogueIS7_S15_Li1ENS1E_22PredicatedTileIteratorINS1E_26OutputTileOptimalThreadMapINS1E_15OutputTileShapeILi128ELi1ELi4ELi4ELi1EEENS1I_ILi1ELi4ELi2ELi1ELi8EEELi256ELi1ELi32EEEfEENS1D_4warp20FragmentIteratorSimtISX_NS1_6thread3MmaINS6_ILi8ELi8ELi1EEEfSL_fSE_fSE_NS_4arch13OpMultiplyAddEbEESE_S13_EENS1N_16TileIteratorSimtISX_S1U_fSE_S13_EENS1E_18SharedLoadIteratorINS1L_18CompactedThreadMapEfLi4EEENS1D_6thread17LinearCombinationIfLi1EffLNS21_9ScaleType4KindE0ELS1A_2EEENSB_ILi0ELi17EEELi1EEENS4_30GemmIdentityThreadblockSwizzleILi1EEELb0EEEEEvNT_6ParamsE")
+    cutlassKern.prepare("320s")
+
+    refKern = mod.get_function("ReferenceGemm_kernel")
+    refKern.prepare("iiifPiPifPi")
+
+    return refKern, cutlassKern
+
+
+class kernelConfig(ct.Structure):
+    _fields_ = [
+        ("gridX", ct.c_int),
+        ("gridY", ct.c_int),
+        ("gridZ", ct.c_int),
+        ("blockX", ct.c_int),
+        ("blockY", ct.c_int),
+        ("blockZ", ct.c_int),
+        ("smem_size", ct.c_int)
+    ]
+
+
+def loadAdapter():
+    libc = ct.cdll.LoadLibrary("./cutlassAdapters.so")
+    getArg = libc.adaptSGEMMArgs
+    c_float_p = ct.POINTER(ct.c_float)
+    getArg.argtypes = [ct.c_int, ct.c_int, ct.c_int, ct.c_float, c_float_p, ct.c_int,
+                       c_float_p, ct.c_int, ct.c_float, c_float_p, ct.c_int]
+    getArg.restype = ct.POINTER(ct.c_byte*320)
+
+    getDims = libc.getCudaConfig
+    # M, N, K
+    getDims.argtypes = [ct.c_int, ct.c_int, ct.c_int]
+    getDims.restype = ct.POINTER(kernelConfig)
+
+    return (getArg, getDims)
+
+
+def testKern():
+    mod = cuda.module_from_file("./cutlass.cubin")
+    kern = mod.get_function("testKernel")
+    kern.prepare("16s")
+
+    lib = ct.cdll.LoadLibrary("./cutlassAdapters.so")
+    getStruct = lib.getTestStruct
+    getStruct.argtypes = [ct.c_int, ct.POINTER(ct.c_float)]
+    getStruct.restype = ct.POINTER(ct.c_byte*16)
+
+    anInt = 42
+    hArr = np.arange(4096*2, dtype=np.float32)
+
+    dArr = cuda.mem_alloc(hArr.nbytes)
+    cuda.memcpy_htod(dArr, hArr)
+
+    arg = getStruct(anInt, ct.cast(int(dArr), ct.POINTER(ct.c_float)))
+
+    kern.prepared_call((1, 1, 1), (1, 1, 1), arg.contents)
+
+
+def testSgemm(M, N, K, alpha, beta):
+    lda = M
+    ldb = K
+    ldc = M
+
+    getArg, getDims = loadAdapter()
+    refKern, cutlassKern = loadKerns()
+
+    rng = np.random.default_rng(0)
+    a = rng.random((M, K), dtype=np.float32)
+    b = rng.random((K, N), dtype=np.float32)
+    c = np.zeros(shape=(M, N), dtype=np.float32)
+
+    a_d = cuda.mem_alloc(a.nbytes)
+    cuda.memcpy_htod(a_d, a)
+
+    b_d = cuda.mem_alloc(b.nbytes)
+    cuda.memcpy_htod(b_d, b)
+
+    c_d = cuda.mem_alloc(c.nbytes)
+    cuda.memset_d8(c_d, 0, c.nbytes)
+
+    cfg = getDims(M, N, K).contents
+    grid = (cfg.gridX, cfg.gridY, cfg.gridZ)
+    block = (cfg.blockX, cfg.blockY, cfg.blockZ)
+
+    print("Grid is: ", grid)
+    print("Block is: ", block)
+    print("Smem Size is: ", cfg.smem_size)
+
+    params = getArg(M, N, K, alpha,
+                    ct.cast(int(a_d), ct.POINTER(ct.c_float)), lda,
+                    ct.cast(int(b_d), ct.POINTER(ct.c_float)), ldb,
+                    beta,
+                    ct.cast(int(c_d), ct.POINTER(ct.c_float)), ldc)
+
+    cutlassKern.prepared_call(grid, block, params.contents, shared_size=cfg.smem_size)
+
+    cuda.memcpy_dtoh(c, c_d)
+
+    refC_d = cuda.mem_alloc(c.nbytes)
+
+    refBlock = (16, 16, 1)
+    refGrid = (((M + 16 - 1) // 16), ((N + 16 - 1) // 16), 1)
+    refKern.prepared_call(refBlock, refGrid, M, N, K, alpha, a_d, lda, b_d, ldb, beta, refC_d, ldc)
+
+    refC_h = np.zeros(shape=(M, N), dtype=np.float32)
+    cuda.memcpy_dtoh(refC_h, refC_d)
+
+    print("Cutlass Kern Result: ")
+    print(c)
+
+    print("Reference Kern Result:")
+    print(refC_h)
+
+    print("NP Result: ")
+    print(np.matmul(a, b))
+
+
+testSgemm(128, 128, 128, 1.0, 0.0)

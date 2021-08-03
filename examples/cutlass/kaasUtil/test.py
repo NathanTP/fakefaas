2fc703cb6945ac10d76ba1b174fbf0a033a6791e
Nathan Pemberton <nathanp@berkeley.edu>
2021-08-03 16:59:14 -0700

    Added some comments to test.py in cutlass

--- examples/cutlass/kaasUtil/test.py
+++ examples/cutlass/kaasUtil/test.py
@@ -6,16 +6,28 @@ import ctypes as ct
 
 def loadKerns():
     mod = cuda.module_from_file("./cutlass.cubin")
+
+    # Since the cutlass kernel came from a template, the name is crazy long.
+    # Unfortunately, extern "C" doesn't fix the issue. This string is obtained
+    # by running "nm" on the cubin
     cutlassKern = mod.get_function("_ZN7cutlass6KernelINS_4gemm6kernel4GemmINS1_11threadblock12MmaPipelinedINS1_9GemmShapeILi128ELi128ELi8EEENS_9transform11threadblock22PredicatedTileIteratorINS_11MatrixShapeILi128ELi8EEEfNS_6layout8RowMajorELi1ENS8_30PitchLinearStripminedThreadMapINSD_16PitchLinearShapeILi8ELi128EEELi256ELi1EEELi1EEENS9_19RegularTileIteratorISC_fNSD_11ColumnMajorELi1ENS8_33TransposePitchLinearThreadMapSimtISI_EELi4EEENSA_INSB_ILi8ELi128EEEfSE_Li0ENSF_INSG_ILi128ELi8EEELi256ELi1EEELi1EEENSK_ISP_fSE_Li0ESR_Li4EEEfSE_NS4_9MmaPolicyINS1_4warp7MmaSimtINS6_ILi32ELi64ELi8EEEfSL_fSE_fSE_NSV_13MmaSimtPolicyINSB_ILi4ELi8EEENSD_19RowMajorInterleavedILi2EEENS6_ILi4ELi4ELi1EEEEELi1ELNS_16ComplexTransformE0ELS14_0EbEENSB_ILi4ELi0EEENSB_ILi0ELi0EEELi1EEENS_21NumericArrayConverterIffLi4ELNS_15FloatRoundStyleE2EEES1B_bEENS_8epilogue11threadblock8EpilogueIS7_S15_Li1ENS1E_22PredicatedTileIteratorINS1E_26OutputTileOptimalThreadMapINS1E_15OutputTileShapeILi128ELi1ELi4ELi4ELi1EEENS1I_ILi1ELi4ELi2ELi1ELi8EEELi256ELi1ELi32EEEfEENS1D_4warp20FragmentIteratorSimtISX_NS1_6thread3MmaINS6_ILi8ELi8ELi1EEEfSL_fSE_fSE_NS_4arch13OpMultiplyAddEbEESE_S13_EENS1N_16TileIteratorSimtISX_S1U_fSE_S13_EENS1E_18SharedLoadIteratorINS1L_18CompactedThreadMapEfLi4EEENS1D_6thread17LinearCombinationIfLi1EffLNS21_9ScaleType4KindE0ELS1A_2EEENSB_ILi0ELi17EEELi1EEENS4_30GemmIdentityThreadblockSwizzleILi1EEELb0EEEEEvNT_6ParamsE")
+
+    # The kernel takes a Params struct as an argument (by value). Rather than
+    # try to define that struct in python, we instead find its size manually
+    # (in cuda using sizeof()) and then specify a byte array of the same size
+    # here. Pycuda doesn't care about the type in practice, it only needs the
+    # size. This type string is defined by python's "struct" module.
     cutlassKern.prepare("320s")
 
     refKern = mod.get_function("ReferenceGemm_kernel")
+    # See python's struct module for a description of this type string
     refKern.prepare("iiifPiPifPi")
 
     return refKern, cutlassKern
 
 
 class kernelConfig(ct.Structure):
+    """This mirrors the CudaConfig struct defined in cutlassAdapters.h"""
     _fields_ = [
         ("gridX", ct.c_int),
         ("gridY", ct.c_int),
@@ -33,6 +45,8 @@ def loadAdapter():
     c_float_p = ct.POINTER(ct.c_float)
     getArg.argtypes = [ct.c_int, ct.c_int, ct.c_int, ct.c_float, c_float_p, ct.c_int,
                        c_float_p, ct.c_int, ct.c_float, c_float_p, ct.c_int]
+    # Instead of trying to define the Params struct in python, we just pretend
+    # that it's a byte array of the same size (320 bytes in this case)
     getArg.restype = ct.POINTER(ct.c_byte*320)
 
     getDims = libc.getCudaConfig
@@ -44,6 +58,8 @@ def loadAdapter():
 
 
 def testKern():
+    """This kernel is useful for trying out ideas without all the complexity of
+    cutlass"""
     mod = cuda.module_from_file("./cutlass.cubin")
     kern = mod.get_function("testKernel")
     kern.prepare("16s")

0cb3644a30bd7e79563684fe06ebf108403ac428
Jason Ding <zhoujie.ding@berkeley.edu>
2021-10-09 01:00:10 +0000

    some changes

--- examples/jacobi/jacobi.cu
+++ examples/jacobi/jacobi.cu
@@ -57,81 +57,131 @@ __device__ double atomicAdd(double *address, double val) {
 #endif
 
 extern "C"
-__global__ void JacobiMethodAllIter(const float *A, const double *b, int n, 
+__global__ void JacobiMethodAllIter(const float *A, const double *b,
         double *x, double *x_new, double *sum,
         int max_iter) 
 {
-    for(int i = 0; i < max_iter; i++) {
-        // Handle to thread block group
-        cg::thread_block cta = cg::this_thread_block();
-        __shared__ double x_shared[N_ROWS];  // N_ROWS == n
-        __shared__ double b_shared[ROWS_PER_CTA + 1];
 
-        for (int i = threadIdx.x; i < N_ROWS; i += blockDim.x) {
-            x_shared[i] = x[i];
-        }
+  cg::thread_block cta = cg::this_thread_block();
+  __shared__ double x_shared[N_ROWS];  // N_ROWS == n
+  __shared__ double b_shared[ROWS_PER_CTA + 1];
 
-        if (threadIdx.x < ROWS_PER_CTA) {
-            int k = threadIdx.x;
-            for (int i = k + (blockIdx.x * ROWS_PER_CTA);
-                (k < ROWS_PER_CTA) && (i < N_ROWS);
-                k += ROWS_PER_CTA, i += ROWS_PER_CTA) {
-            b_shared[i % (ROWS_PER_CTA + 1)] = b[i];
-            }
-        }
+  for (int i = threadIdx.x; i < N_ROWS; i += blockDim.x) {
+    x_shared[i] = x[i];
+  }
 
-        cg::sync(cta);
-
-        cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cta);
-
-        for (int k = 0, i = blockIdx.x * ROWS_PER_CTA;
-            (k < ROWS_PER_CTA) && (i < N_ROWS); k++, i++) {
-            double rowThreadSum = 0.0;
-            for (int j = threadIdx.x; j < N_ROWS; j += blockDim.x) {
-            rowThreadSum += (A[i * N_ROWS + j] * x_shared[j]);
-            }
-
-            for (int offset = tile32.size() / 2; offset > 0; offset /= 2) {
-            rowThreadSum += tile32.shfl_down(rowThreadSum, offset);
-            }
-
-            if (tile32.thread_rank() == 0) {
-            atomicAdd(&b_shared[i % (ROWS_PER_CTA + 1)], -rowThreadSum);
-            }
-        }
-
-        cg::sync(cta);
-
-        if (threadIdx.x < ROWS_PER_CTA) {
-            cg::thread_block_tile<ROWS_PER_CTA> tile8 =
-                cg::tiled_partition<ROWS_PER_CTA>(cta);
-            double temp_sum = 0.0;
-
-            int k = threadIdx.x;
-
-            for (int i = k + (blockIdx.x * ROWS_PER_CTA);
-                (k < ROWS_PER_CTA) && (i < N_ROWS);
-                k += ROWS_PER_CTA, i += ROWS_PER_CTA) {
-            double dx = b_shared[i % (ROWS_PER_CTA + 1)];
-            dx /= A[i * N_ROWS + i];
-
-            x_new[i] = (x_shared[i] + dx);
-            temp_sum += fabs(dx);
-            }
-
-            for (int offset = tile8.size() / 2; offset > 0; offset /= 2) {
-            temp_sum += tile8.shfl_down(temp_sum, offset);
-            }
-
-            if (tile8.thread_rank() == 0) {
-            atomicAdd(sum, temp_sum);
-            }
-        }
-
-        __syncthreads();
-        double *tmp;
-        tmp = x;
-        x = x_new;
-        x_new = tmp;
+  if (threadIdx.x < ROWS_PER_CTA) {
+    int k = threadIdx.x;
+    for (int i = k + (blockIdx.x * ROWS_PER_CTA);
+        (k < ROWS_PER_CTA) && (i < N_ROWS);
+        k += ROWS_PER_CTA, i += ROWS_PER_CTA) {
+      b_shared[i % (ROWS_PER_CTA + 1)] = b[i];
     }
+  }
+
+  cg::sync(cta);
+
+  cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cta);
+
+  for (int k = 0, i = blockIdx.x * ROWS_PER_CTA;
+      (k < ROWS_PER_CTA) && (i < N_ROWS); k++, i++) {
+    double rowThreadSum = 0.0;
+    for (int j = threadIdx.x; j < N_ROWS; j += blockDim.x) {
+      rowThreadSum += (A[i * N_ROWS + j] * x_shared[j]);
+    }
+
+    for (int offset = tile32.size() / 2; offset > 0; offset /= 2) {
+      rowThreadSum += tile32.shfl_down(rowThreadSum, offset);
+    }
+
+    if (tile32.thread_rank() == 0) {
+      atomicAdd(&b_shared[i % (ROWS_PER_CTA + 1)], -rowThreadSum);
+    }
+  }
+
+  cg::sync(cta);
+
+  if (threadIdx.x < ROWS_PER_CTA) {
+    cg::thread_block_tile<ROWS_PER_CTA> tile8 =
+        cg::tiled_partition<ROWS_PER_CTA>(cta);
+    double temp_sum = 0.0;
+
+    int k = threadIdx.x;
+
+    for (int i = k + (blockIdx.x * ROWS_PER_CTA);
+        (k < ROWS_PER_CTA) && (i < N_ROWS);
+        k += ROWS_PER_CTA, i += ROWS_PER_CTA) {
+      double dx = b_shared[i % (ROWS_PER_CTA + 1)];
+      dx /= A[i * N_ROWS + i];
+
+      x_new[i] = (x_shared[i] + dx);
+      temp_sum += fabs(dx);
+    }
+
+    for (int offset = tile8.size() / 2; offset > 0; offset /= 2) {
+      temp_sum += tile8.shfl_down(temp_sum, offset);
+    }
+
+    if (tile8.thread_rank() == 0) {
+      atomicAdd(sum, temp_sum);
+    }
+  }
+}
+
+  //   __syncthreads();
+  //   double *tmp;
+  //   tmp = x;
+  //   x = x_new;
+  //   x_new = tmp;
+  // }
+
+// extern "C"
+// __global__ void JacobiMethodOuter(const float *A, const double *b,
+//                                     double *x,
+//                                     double *x_new, double *sum, const int max_iter)
+// {
+//   dim3 nthreads(256, 1, 1);
+//   // grid size
+//   dim3 nblocks((N_ROWS / ROWS_PER_CTA) + 2, 1, 1);
+//   for (int iter = 0; iter < max_iter; iter++) {
+//     if (iter & 1 == 0) {
+//       JacobiMethodAllIter<<<nthreads, nblocks, 0, stream>>>(A, b, x, x_new, sum, max_iter);
+//     } else {
+//       JacobiMethodAllIter<<<nthreads, nblocks, 0, stream>>>(A, b, x_new, x, sum, max_iter);
+//     }
+//   }
+// }
+
+
+double JacobiMethodGpu(const float *A, const double *b,
+                       const float conv_threshold, const int max_iter,
+                       double *x, double *x_new, cudaStream_t stream) {
+  // CTA size
+  dim3 nthreads(256, 1, 1);
+  // grid size
+  dim3 nblocks((N_ROWS / ROWS_PER_CTA) + 2, 1, 1);
+
+  double sum = 10.0;
+  double *d_sum;
+  checkCudaErrors(cudaMalloc(&d_sum, sizeof(double)));
+  int k = 0;
+
+  // JacobiMethodOuter<<<1, 1>>>(A, b, x, x_new, d_sum, max_iter);
+  for (int iter = 0; iter < max_iter; iter++) {
+    if (iter & 1 == 0) {
+      JacobiMethodAllIter<<<nthreads, nblocks, 0, stream>>>(A, b, x, x_new, d_sum, max_iter);
+    } else {
+      JacobiMethodAllIter<<<nthreads, nblocks, 0, stream>>>(A, b, x_new, x, d_sum, max_iter);
+    }
+    // JacobiMethodAllIter<<<nthreads, nblocks, 0, stream>>>(A, b, x, x_new, d_sum, max_iter);
+  }
+  checkCudaErrors(cudaMemcpyAsync(&sum, d_sum, sizeof(double),
+                              cudaMemcpyDeviceToHost, stream));
+  checkCudaErrors(cudaStreamSynchronize(stream));
+
+  printf("GPU iterations : %d\n", k + 1);
+  printf("GPU error : %.3e\n", sum);
+
+  checkCudaErrors(cudaFree(d_sum));
+  return sum;
 }
--- examples/jacobi/test.py
+++ examples/jacobi/test.py
@@ -9,23 +9,39 @@ ROWS_PER_CTA = 8
 
 def loadKerns():
     mod = cuda.module_from_file("jacobi.ptx")
-    jacobiKern = mod.get_function("JacobiMethodAllIter")
-    jacobiKern.prepare("PPiPPPi")
+    jacobiKern = mod.get_function("JacobiMethodOuter")
+    jacobiKern.prepare("PPPPPi")
 
     return jacobiKern
 
+def test(A, b, N, iters):
+    x = np.zeros((len(A[0]), 1))
+    x_new = np.zeros_like(x)
+    for k in range(iters):
+        for i in range(N):
+            temp = b[i]
+            for j in range(N):
+                temp -= A[i, j] * x[j]
+                temp /= A[i, i]
+                x_new[i] += temp
+        x = np.copy(x_new)
+    return x
 
-class kernelConfig(ct.Structure):
-    """This mirrors the CudaConfig struct defined in cutlassAdapters.h"""
-    _fields_ = [
-        ("gridX", ct.c_int),
-        ("gridY", ct.c_int),
-        ("gridZ", ct.c_int),
-        ("blockX", ct.c_int),
-        ("blockY", ct.c_int),
-        ("blockZ", ct.c_int),
-        ("smem_size", ct.c_int)
-    ]
+def np_jacobi(A,b,N,x=None):
+    """Solves the equation Ax=b via the Jacobi iterative method."""
+    # Create an initial guess if needed                                                                                                                                                            
+    if x is None:
+        x = np.zeros((len(A[0]), 1))
+
+    # Create a vector of the diagonal elements of A                                                                                                                                                
+    # and subtract them from A                                                                                                                                                                     
+    D = np.diagflat(np.diag(A))
+    R = A - D
+
+    # Iterate for N times                                                                                                                                                                          
+    for i in range(N):
+        x = np.linalg.inv(D) @ (b - (R @ x))
+    return x
 
 # @Params:
 # N: Number of rows (columns); iters: Maximum iterations
@@ -33,14 +49,21 @@ def testJacobi(N, iters):
 
     jacobiKern = loadKerns()
 
-    rng = np.random.default_rng(50)
-    A = rng.random((N, N), dtype=np.float32)
-    fill_arr = np.sum(np.abs(A), axis=1) + 1
-    np.fill_diagonal(A, fill_arr)
-    b = rng.random((N, 1), dtype=np.float64)
+    # rng = np.random.default_rng(40)
+    # A = rng.random((N, N), dtype=np.float32)
+    # fill_arr = np.sum(np.abs(A), axis=1)
+    # np.fill_diagonal(A, fill_arr)
+    # b = rng.random((N, 1), dtype=np.float64)
+    A = np.zeros(shape=(N, N), dtype=np.float32)
+    b = np.zeros(shape=(N, 1), dtype=np.float64)
+    for i in range(N):
+        b[i] = 2 * N
+        for j in range(N):
+            A[i, j] = 1
+            A[i, i] = N + 1
     x = np.zeros(shape=(N, 1), dtype=np.float64)
     x_new = np.zeros(shape=(N, 1), dtype=np.float64)
-    d_sum = np.zeros(1, dtype=np.float64)
+    d = np.zeros(1, dtype=np.float64)
 
     A_d = cuda.mem_alloc(A.nbytes)
     cuda.memcpy_htod(A_d, A)
@@ -63,16 +86,32 @@ def testJacobi(N, iters):
     print("Grid is: ", grid)
     print("Block is: ", block)
 
-    jacobiKern.prepared_call(grid, block, A_d, b_d, N, x_d, x_new_d, d_d, iters)
+    jacobiKern.prepared_call(grid, block, A_d, b_d, x_d, x_new_d, d_d, iters)
+
+    # np_act = np.linalg.solve(A, b)
+    # np_res = np_jacobi(A, b, iters)
+    # tes = test(A, b, N, iters)
+
+    # print(np.linalg.norm(np.abs((np_act - np_res) / np_res)))
 
     if iters % 2 == 0:
-        # x_new
         cuda.memcpy_dtoh(x_new, x_new_d)
-        print(x_new)
+        print("CUDA result is:")
+        print(sum(x_new))
+        # print(np_res)
+        # err = np.linalg.norm(np.abs((x_new - np_res) / np_res))
+        # print("error is:")
+        # print(err)
+
     else:
         cuda.memcpy_dtoh(x, x_d)
+        print("CUDA result is:")
         print(x)
+        # err = np.linalg.norm(np.abs((x - np_res) / np_res))
+        # print("error is:")
+        # print(err)
 
-testJacobi(512, 1000)
-# mod = cuda.module_from_file("jacobi.ptx")
-# kern = loadKerns()
+    cuda.memcpy_dtoh(d, d_d)
+    print(d)
+
+testJacobi(512, 3000)
